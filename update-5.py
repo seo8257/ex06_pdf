9b0782098fd1f4877d92e3425d1ed382ad00cd47
seo8257 <barrac7@gmail.com>
2026-06-11 14:29:47 +0900

    commit
    commit

--- update-5.py
+++ update-5.py
@@ -22,8 +22,8 @@ from langchain_chroma import Chroma
 from langchain_openai import ( OpenAIEmbeddings,   ChatOpenAI )
 
 # Retrieval Chain
-from langchain.chains import (   create_retrieval_chain )
-from langchain.chains.combine_documents import (   create_stuff_documents_chain )
+from langchain_classic.chains import create_retrieval_chain
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
 
 # Prompt
 from langchain_core.prompts import (  ChatPromptTemplate )
