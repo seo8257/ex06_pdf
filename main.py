9a110317f4907eebeba44d795f90aa63b2dd3e49
seo8257 <barrac7@gmail.com>
2026-06-11 17:06:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import tempfile
 import streamlit as st
 from langchain_community.document_loaders import (  PyPDFLoader )
 from langchain_text_splitters import (    RecursiveCharacterTextSplitter )
-from langchain_core.vectorstores import InMemoryVectorStore
+from langchain_core.vectorstores import InMemoryVectorStore   
 from langchain_openai import   OpenAIEmbeddings,    ChatOpenAI 
 from langchain_classic.chains import (   create_retrieval_chain )
 from langchain_classic.chains.combine_documents import create_stuff_documents_chain
