e6fb51733f3b7a8c270e2043359ce876918fbca9
seo8257 <barrac7@gmail.com>
2026-06-11 17:11:39 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import os
 import tempfile
 
 import streamlit as st
-from langchain_community.document_loaders import (  PyPDFLoader )
+from pypdf import PdfReader
+from langchain_core.documents import Document
 from langchain_text_splitters import (    RecursiveCharacterTextSplitter )
 from langchain_core.vectorstores import InMemoryVectorStore   
 from langchain_openai import   OpenAIEmbeddings,    ChatOpenAI 
